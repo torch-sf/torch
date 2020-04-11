c86a39d69cabe62eca36d97bb3273a386997d6cd
Aaron Tran <aaron.tran@columbia.edu>
2020-04-11 15:32:15 -0400

    docs: cleanup torch quickstart/manual files

--- docs/torch_manual/post_atran.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# Meant for use by A Tran only, to post to web
-# in order to prevent search engine crawlers it is weakly password protected
-rsync -avh main.pdf pluto:~/www/torch/manual.pdf
