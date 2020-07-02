c66644871ffd431a54ed5564643abac8a061b061
Aaron Tran <aaron.tran@columbia.edu>
2020-07-02 02:20:13 -0400

    Add mm-dd-YYYY to torch timestamp

--- src/torch_stdout.py
+++ src/torch_stdout.py
@@ -5,9 +5,7 @@ from __future__ import division, print_function
 from datetime import datetime
 
 def tprint(*args, **kwargs):
-    # option 1
-    #print("[torch] ", end='')
-    # option 2
-    tstr = datetime.now().strftime("%H:%M:%S.%f")
+    #tstr = datetime.now().strftime("%H:%M:%S.%f")
+    tstr = (datetime.now().strftime("%m-%d-%Y %H:%M:%S.%f"))[:-3]
     print("[torch {}] ".format(tstr), end='')
     return print(*args, **kwargs)
