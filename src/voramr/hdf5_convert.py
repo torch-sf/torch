bb42cc8f9174b7ff6546a8a2c0e3ac7a1be382ee
Sean Lewis <sean.phys@gmail.com>
2023-02-27 17:48:56 +0100

    local particle refinement

--- src/voramr/hdf5_convert.py
+++ src/voramr/hdf5_convert.py
@@ -66,7 +66,7 @@ def rescale_coords_vels(coords, vels, masses, scoords, svels, apply_consts=True,
     vx_cor = com_vels[0]
     vy_cor = com_vels[1]
     vz_cor = com_vels[2]
-    
+
     coords_cor = coords - np.array([x_cor, y_cor, z_cor]).reshape(1,3)
     vels_cor = vels - np.array([vx_cor, vy_cor, vz_cor]).reshape(1,3)
 
@@ -75,16 +75,38 @@ def rescale_coords_vels(coords, vels, masses, scoords, svels, apply_consts=True,
     return coords_cor, vels_cor, scoords_cor, svels_cor
 
 def write_corrected_file(output_filename, coords, vels, dens, masses, ie, gpot,
-                         scoords, svels, smass, sinitmass, sfmtime, smetal):
+                         scoords, svels, smass, sinitmass, sfmtime, smetal, local_ref=None):
     f = h5py.File(output_filename, 'w')
     # Recreate gas dataset
     group = f.create_group('PartType0')
-    dset = group.create_dataset('Coordinates', data=coords, dtype='d')
-    dset = group.create_dataset('Velocities', data=vels, dtype='d')
-    dset = group.create_dataset('Density', data=dens, dtype='d')
-    dset = group.create_dataset('Masses', data=masses, dtype='d')
-    dset = group.create_dataset('InternalEnergy', data=ie, dtype='d')
-    dset = group.create_dataset('Potential', data=gpot, dtype='d')
+    
+    if(local_ref):
+        print("DOING LOCALIZED REFINEMENT. Limiting gas particles written.")
+        print("locx = ", local_ref[0])
+        print("locy = ", local_ref[1])
+        print("locz = ", local_ref[2])
+        print("locr = ", local_ref[3])
+        locx, locy, locz, locr = local_ref[0], local_ref[1], local_ref[2], local_ref[3]
+        diffr = np.sqrt((coords[:,0]-locx)**2 + (coords[:,1]-locy)**2 + (coords[:,2]-locz)**2)
+        ind = np.where(diffr < locr)
+        print("INDICIES < locr:", ind)
+        print("Coords shape: ", coords[ind].shape)
+        print("masses shape: ", masses[ind].shape)
+        dset = group.create_dataset('Coordinates', data=coords[ind], dtype='d')
+        dset = group.create_dataset('Velocities', data=vels[ind], dtype='d')
+        dset = group.create_dataset('Density', data=dens[ind], dtype='d')
+        dset = group.create_dataset('Masses', data=masses[ind], dtype='d')
+        dset = group.create_dataset('InternalEnergy', data=ie[ind], dtype='d')
+        dset = group.create_dataset('Potential', data=gpot[ind], dtype='d')
+        
+    else:
+        print("USING ALL PARTICLES, NO LOCAL REF.")
+        dset = group.create_dataset('Coordinates', data=coords, dtype='d')
+        dset = group.create_dataset('Velocities', data=vels, dtype='d')
+        dset = group.create_dataset('Density', data=dens, dtype='d')
+        dset = group.create_dataset('Masses', data=masses, dtype='d')
+        dset = group.create_dataset('InternalEnergy', data=ie, dtype='d')
+        dset = group.create_dataset('Potential', data=gpot, dtype='d')
 
     # Recreate stars dataset
     group_s = f.create_group('PartType4')
