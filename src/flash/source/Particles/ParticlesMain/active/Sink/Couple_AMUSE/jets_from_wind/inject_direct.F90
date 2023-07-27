fbd5a755dd00eda103c191ef3d8ff51ac27b75c0
Sabrina Appel <appel@physics.rutgers.edu>
2023-07-26 20:10:42 -0400

    inject_direct.F90: add jet_wind switch to inject_direct call and if statement around jet calc

--- src/flash/source/Particles/ParticlesMain/active/Sink/Couple_AMUSE/jets_from_wind/inject_direct.F90
+++ src/flash/source/Particles/ParticlesMain/active/Sink/Couple_AMUSE/jets_from_wind/inject_direct.F90
@@ -35,7 +35,7 @@
 
 
 
-subroutine inject_direct(loc_in, angmom_in, injectMassIn, injectVelocityIn, starMass, twind, dt, bgDens)
+subroutine inject_direct(loc_in, angmom_in, jet_wind, injectMassIn, injectVelocityIn, starMass, twind, dt, bgDens)
 ! Add angular momentum as input param -SA 20230718
 
 !#define DEBUG
@@ -76,6 +76,7 @@ integer, parameter :: dp = kind(1.d0)
 
 real(dp), intent(in)    :: loc_in(3)
 real(dp), intent(in)    :: angmom_in(3)
+character(len = 4)      :: jet_wind !Added -SA 20230726
 real(dp), intent(in)    :: injectMassIn, injectVelocityIn, twind, dt
 real(dp), intent(inout) :: bgDens
 
@@ -594,74 +595,88 @@ print *, "Found", injBlkNum, "injection blocks on proc ", gr_meshMe
                     rad2 = dx**2 + dy**2 + dz**2
                     rad  = sqrt(rad2)
 
-                    !!!  Test accessing new angular momentum property:
-                    j_x = angmom_in(1)
-                    j_y = angmom_in(2)
-                    j_z = angmom_in(3)
-                    print*, "Test accessing angular momentum: ", angmom_in, [j_x, j_y, j_z]
+                    if (jet_wind .eq. "jet") then
+                        print,* "inject_direct.F90: injecting jet"
 
-                    ang_mom_mag = sqrt( j_x**2 + j_y**2 + j_z**2)
-                    print*, "inject_direct.F90: angular momentum magnitude: ", ang_mom_mag
+                        !!!  Test accessing new angular momentum property:
+                        j_x = angmom_in(1)
+                        j_y = angmom_in(2)
+                        j_z = angmom_in(3)
+                        print*, "Test accessing angular momentum: ", angmom_in, [j_x, j_y, j_z]
 
+                        ang_mom_mag = sqrt( j_x**2 + j_y**2 + j_z**2)
+                        print*, "inject_direct.F90: angular momentum magnitude: ", ang_mom_mag
 
-                    !!!  Set up a new set of x,y,z coord axes for the jet - the box axes with an offset.
-                    !!!  Then we can define a new r and theta for the jet to calculate ang_dependence. -SA 1/23/2022
-                    !!!  theta_z is the rotation about the x-axis to get the position of the jet, etc.
-                    theta_x = 0.0  !0.78  !!is roughly pi/4
-                    theta_y = 1.0  !1 radian ~ 60 degrees; angle previously was: ! 0.4
-                    theta_z = 0.0  !1.57
+                        !!!  Set up a new set of x,y,z coord axes for the jet - the box axes with an offset.
+                        !!!  Then we can define a new r and theta for the jet to calculate ang_dependence. -SA 1/23/2022
+                        !!!  theta_z is the rotation about the x-axis to get the position of the jet, etc.
+                        ! theta_x = 0.0  !0.78  !!is roughly pi/4
+                        ! theta_y = 1.0  !1 radian ~ 60 degrees; angle previously was: ! 0.4
+                        ! theta_z = 0.0  !1.57
 
-                    dx_jet = (cos(theta_z)*cos(theta_y))*dx + &
-                         (cos(theta_z)*sin(theta_y)*sin(theta_x) - sin(theta_z)*cos(theta_x))*dy + &
-                         (sin(theta_z)*sin(theta_x) + cos(theta_z)*sin(theta_y)*cos(theta_x))*dz
-                    dy_jet = (sin(theta_z)*cos(theta_y))*dx + &
-                         (sin(theta_z)*sin(theta_y)*sin(theta_x) + cos(theta_z)*cos(theta_x))*dy + &
-                         (sin(theta_z)*sin(theta_y)*cos(theta_x) - cos(theta_z)*sin(theta_x))*dz
-                    dz_jet = -sin(theta_y)*dx + (cos(theta_y)*sin(theta_x))*dy + &
-                         (cos(theta_y)*cos(theta_x))*dz
+                        ! dx_jet = (cos(theta_z)*cos(theta_y))*dx + &
+                            ! (cos(theta_z)*sin(theta_y)*sin(theta_x) - sin(theta_z)*cos(theta_x))*dy + &
+                            ! (sin(theta_z)*sin(theta_x) + cos(theta_z)*sin(theta_y)*cos(theta_x))*dz
+                        ! dy_jet = (sin(theta_z)*cos(theta_y))*dx + &
+                            ! (sin(theta_z)*sin(theta_y)*sin(theta_x) + cos(theta_z)*cos(theta_x))*dy + &
+                            ! (sin(theta_z)*sin(theta_y)*cos(theta_x) - cos(theta_z)*sin(theta_x))*dz
+                        ! dz_jet = -sin(theta_y)*dx + (cos(theta_y)*sin(theta_x))*dy + &
+                            ! (cos(theta_y)*cos(theta_x))*dz
 
-                    rad2_jet = dx_jet**2.0 + dy_jet**2.0 + dz_jet**2.0
-                    rad_jet = sqrt(rad2_jet)  !! This should be the same as rad (defined above).  Test this.
+                        rad2_jet = dx_jet**2.0 + dy_jet**2.0 + dz_jet**2.0
+                        rad_jet = sqrt(rad2_jet)  !! This should be the same as rad (defined above).  Test this.
 
-                    theta = acos(dz_jet/rad_jet)
-                    phi = atan(dy_jet/dx_jet)
+                        theta = acos(dz_jet/rad_jet)
+                        phi = atan(dy_jet/dx_jet)
 
-                    ang_dependence = (cos(theta))**2.0  !! A cos^2 dependence
-                    !!! This is just to test the overall set up.  We'll need to change this later to get the
-                    !!! Cunningham model set up. -SA 1/23/2022
+                        ang_dependence = (cos(theta))**2.0  !! A cos^2 dependence
+                        !!! This is just to test the overall set up.  We'll need to change this later to get the
+                        !!! Cunningham model set up. -SA 1/23/2022
 
-                    !!!  Let's start building the pieces for the Cunningham model. -SA 2/7/22
-                    c_one = 1.0
-                    c_two = 1.0
-                    norm_factor = 1.0
+                        !!!  Let's start building the pieces for the Cunningham model. -SA 2/7/22
+                        c_one = 1.0
+                        c_two = 1.0
+                        norm_factor = 1.0
 
-                    ave_delta = SUM(delta)/3   !!  Not sure if this is what we want ultimately, but should let the code run.
-                    delta_theta = atan(ave_delta/rad_jet) !atan(1.0/8.0) !! This is from Cunningham?  UPDATE!
+                        ave_delta = SUM(delta)/3   !!  Not sure if this is what we want ultimately, but should let the code run.
+                        delta_theta = atan(ave_delta/rad_jet) !atan(1.0/8.0) !! This is from Cunningham?  UPDATE!
 
-                    theta_zero = 0.01
+                        theta_zero = 0.01
                     
-                    if (abs(sin(3.14159265359/2.0 - theta)) .ge. ave_delta/rad_jet) then  ! Eq. 21 from Cunningham - if statement added 20220825
-                        ang_dependence = (1.0/c_two) * (1.0/delta_theta) * (1.0/(theta_zero*sqrt(1.0+theta_zero**2.0) )) * &
-                            ( atan( (sqrt(1.0+theta_zero**2.0)*tan(theta + delta_theta/2.0) )/theta_zero)  -  &
-                            atan( (sqrt(1.0+theta_zero**2.0)*tan(theta - delta_theta/2.0) )/theta_zero) )
-                    else
-                        ang_dependence = 0.0  !!  This sets the gap at the equator
-                    endif
+                        if (abs(sin(3.14159265359/2.0 - theta)) .ge. ave_delta/rad_jet) then  ! Eq. 21 from Cunningham - if statement added 20220825
+                            ang_dependence = (1.0/c_two) * (1.0/delta_theta) * (1.0/(theta_zero*sqrt(1.0+theta_zero**2.0) )) * &
+                                ( atan( (sqrt(1.0+theta_zero**2.0)*tan(theta + delta_theta/2.0) )/theta_zero)  -  &
+                                atan( (sqrt(1.0+theta_zero**2.0)*tan(theta - delta_theta/2.0) )/theta_zero) )
+                        else
+                            ang_dependence = 0.0  !!  This sets the gap at the equator
+                        endif
 
-                    if (4*ave_delta .lt. rad_jet .AND. rad_jet .lt. 8*ave_delta) then   !!  This assumes delta=delta_x from Cunningham.
-                       !!!  I am pretty sure that delta is the min cell size - and I think this is what we want here.
-                       rad_dependence = (1/c_one) * rad_jet**(-2.0)
-                    else
-                       rad_dependence = 0.0  !!  This both introduces the gap right next to the star particles and
+                        if (4*ave_delta .lt. rad_jet .AND. rad_jet .lt. 8*ave_delta) then   !!  This assumes delta=delta_x from Cunningham.
+                            !!!  I am pretty sure that delta is the min cell size - and I think this is what we want here.
+                            rad_dependence = (1/c_one) * rad_jet**(-2.0)
+                        else
+                            rad_dependence = 0.0  !!  This both introduces the gap right next to the star particles and
                                              !!  sets the outer limit of the injection region. 
-                    endif
+                        endif
 
-                    !! ang_dependence = (cos(theta))**2.0  !!  Keep the cos^2 for now
+                        !! ang_dependence = (cos(theta))**2.0  !!  Keep the cos^2 for now
                     
                     
-                    !print*, "cos^2 is:", (cos(theta))**2.0 
-                    !print*, "But Cunningham ang dependence is: ", ang_dependence
-                    !print*, "Also, the radial dependence is:", rad_dependence
+                        !print*, "cos^2 is:", (cos(theta))**2.0 
+                        !print*, "But Cunningham ang dependence is: ", ang_dependence
+                        !print*, "Also, the radial dependence is:", rad_dependence
+
+                    else if (jet_wind .eq. "wind") then  ! -SA 20230726
+                        print*, "inject_direct.F90: injecting wind"
+                        ang_dependence = 1.0
+                        rad_depandence = 1.0 
+                        !Multiplying by 1 changes nothing so this should return inject_direct.F90 to the default spherical wind.
+
+                    else ! -SA 20230726
+                        print*, "inject_direct.F90: Don't know what to inject!!!  Help! (Assuming wind...)"
+                        ang_dependence = 1.0
+                        rad_depandence = 1.0
+                        !Multiplying by 1 changes nothing so this should return inject_direct.F90 to the default spherical wind.
 
                     ! normalized components of the star --> cell center vector 
                     if (rad .ne. 0.0_dp) then
