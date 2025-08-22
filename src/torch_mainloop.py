91ab20ddc44cdeec2dc366742e7e251c71b02e1d
Brooke Polak <bp4928@gmail.com>
2025-08-22 13:33:55 -0500

    removed non-voramr related changes

--- src/torch_mainloop.py
+++ src/torch_mainloop.py
@@ -128,7 +128,7 @@ def initialize_workers():
         grav.parameters.timestep_parameter = 0.14  # timestep accuracy # TODO how was this chosen?! -AT,2019oct13
     elif USER['with_petar']:
         grav = Petar(convert, number_of_workers=USER['num_grav_workers'], mode='cpu', redirection='none')
-        #grav.parameters.epsilon_squared = USER['epsilon']**2.0
+        grav.parameters.epsilon_squared = USER['epsilon']**2.0
         grav.parameters.r_out = USER['petar_rout']
     else:
         grav = Hermite(convert, number_of_workers=USER['num_grav_workers'], redirection='none')
@@ -343,8 +343,7 @@ def evolve(state, hydro, grav, mult, se):
 
                     else:
                         req_hydro = hydro.evolve_model.asynchronous(hy_time+dt)
-                        if USER['with_petar']:
-                            grav.parameters.dt_soft = dt
+                        grav.parameters.dt_soft = dt
                         dt_old = dt
                         req_grav = grav.evolve_model.asynchronous(hy_time+dt)
                         pool.add_request(req_hydro, handle_result, ["hydro", it])
