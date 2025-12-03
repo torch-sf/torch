6e3488e8bb766a6284b2ae0a43653073ecc73d62
Lourens Veen <l.veen@esciencecenter.nl>
2025-12-03 20:44:07 +0100

    Use print0 everywhere and simplify slightly

--- relink.sh
+++ relink.sh
@@ -25,33 +25,14 @@ if [ ! -d "${FLASH_DIR}/source/flashUtilities" ] ; then
     exit 1
 fi
 
-# macOS fix: printf does not work for macOS
-if find --version >/dev/null 2>&1 ; then
-    FIND_CMD='find "${TORCH_DIR}/src/flash" -type f -printf "%P\0"'
-    BACKUP_FIND='find "${FLASH_DIR}" -name "*.replaced-by-torch" -printf "%P\0"'
-    IS_GNU=1
-else
-    echo
-    echo "System is not GNU, will try alternative solution for find"
-    FIND_CMD='find "${TORCH_DIR}/src/flash" -type f -print0'
-    BACKUP_FIND='find "${FLASH_DIR}" -name "*.replaced-by-torch" -print0'
-    IS_GNU=0
-fi
-
 echo
 echo "Relinking Torch Fortran files into FLASH..."
 
 # Link Torch files into the FLASH directory
 # Existing files will be renamed and replaced by a link
 while IFS= read -r -d $'\0' file <&3; do
-    
-    # macOS fix: strip path prefix
-    if [ "$IS_GNU" -eq 0 ]; then
-        file="${file#${TORCH_DIR}/src/flash/}"
-    fi
-
-    source="$(realpath "${TORCH_DIR}/src/flash/${file}")"
-    target="${FLASH_DIR}/${file}"
+    source="$(realpath "${file}")"
+    target="${FLASH_DIR}/${file#${TORCH_DIR}/src/flash/}"
     mkdir -p "$(dirname "${target}")"
     if [ ! -L "${target}" ] ; then
         if [ -e "${target}" ] ; then
@@ -59,22 +40,16 @@ while IFS= read -r -d $'\0' file <&3; do
         fi
         ln -s "${source}" "${target}"
     fi
-done 3< <(eval "$FIND_CMD")
+done 3< <(find "${TORCH_DIR}"/src/flash -type f -print0)
 
 
 # Restore original FLASH file if the Torch version no longer exists
 while IFS= read -r -d $'\0' file <&3; do
-
-    # macOS fix: strip path prefix
-    if [ "$IS_GNU" -eq 0 ]; then
-        file="${file#${FLASH_DIR}/}"
-    fi
-
-    backup="$(realpath "${FLASH_DIR}/${file}")"
+    backup="$(realpath "${file}")"
     link="${backup%.replaced-by-torch}"
     if [ ! -e "${link}" ] ; then
         rm -f "${link}"
         mv "${backup}" "${link}"
     fi
-done 3< <(eval "$BACKUP_FIND")
+done 3< <(find "${FLASH_DIR}" -name '*.replaced-by-torch' -print0)
 
