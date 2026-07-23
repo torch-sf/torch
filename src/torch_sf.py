2a97fcf0588876aa3fba9b1c79bda793cf954242
Eric Andersson <44900235+astro-ericand@users.noreply.github.com>
2026-07-23 09:47:04 -0400

    fixed indentation in torch_sf.py

--- src/torch_sf.py
+++ src/torch_sf.py
@@ -286,7 +286,7 @@ def queue_stars(state, hydro, min_imf_mass=None, max_imf_mass=None,
                                           min_samp_mass=min_imf_mass.value_in(units.MSun),
                                           max_samp_mass=max_imf_mass.value_in(units.MSun),
                                           sum_small=sum_small,
-					  m_small=m_small.value_in(units.MSun)
+										  m_small=m_small.value_in(units.MSun)
                                           )
             
                 tprint("... sink tag {}".format(sink_tag), end='')
